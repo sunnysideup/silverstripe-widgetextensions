ece8d008174cd4f10d24b60a20407b2cafd3997a
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 14:01:40 +1300

    PATCH: module clean-up

--- _config.php
+++ _config.php
@@ -7,7 +7,3 @@
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START widgetextensions MODULE ----------------===================
 //===================---------------- END widgetextensions MODULE ----------------===================
-
-
-
-
--- code/WidgetAreaEditorExtended.php
+++ code/WidgetAreaEditorExtended.php
@@ -5,47 +5,52 @@
  * @subpackage content
  * @author nicolaas [at] sunnysideup.co.nz
  */
-class WidgetAreaEditorExtended extends WidgetAreaEditor {
+class WidgetAreaEditorExtended extends WidgetAreaEditor
+{
+    protected $availableWidgets = array();
 
-	protected $availableWidgets = array();
+    protected $blockedWidgets = array();
 
-	protected $blockedWidgets = array();
+    public function addToAvailableWidgets($array)
+    {
+        if (!is_array($array)) {
+            user_error("first and only argument for WidgetAreaEditor::addToAvailableWidgets must be an array", E_USER_ERROR);
+        }
+        $this->availableWidgets = $array;
+    }
 
-	function addToAvailableWidgets($array) {
-		if(!is_array($array)) {user_error("first and only argument for WidgetAreaEditor::addToAvailableWidgets must be an array", E_USER_ERROR);}
-		$this->availableWidgets = $array;
-	}
+    public function blockFromAvailableWidgets($array)
+    {
+        if (!is_array($array)) {
+            user_error("first and only argument for WidgetAreaEditor::blockFromAvailableWidgets must be an array", E_USER_ERROR);
+        }
+        $this->blockedWidgets = $array;
+    }
 
-	function blockFromAvailableWidgets($array) {
-	if(!is_array($array)) {user_error("first and only argument for WidgetAreaEditor::blockFromAvailableWidgets must be an array", E_USER_ERROR);}
-		$this->blockedWidgets = $array;
-	}
+    public function AvailableWidgets()
+    {
+        $classes = ClassInfo::subclassesFor('Widget');
+        array_shift($classes);
+        $widgets= new ArrayList();
+        $hasSpecificallyAddedWidgets = count($this->availableWidgets) && is_array($this->availableWidgets);
+        $hasSpecificallyBlockedWidgets = count($this->blockedWidgets) && is_array($this->blockedWidgets);
 
-	function AvailableWidgets() {
-		$classes = ClassInfo::subclassesFor('Widget');
-		array_shift($classes);
-		$widgets= new ArrayList();
-		$hasSpecificallyAddedWidgets = count($this->availableWidgets) && is_array($this->availableWidgets);
-		$hasSpecificallyBlockedWidgets = count($this->blockedWidgets) && is_array($this->blockedWidgets);
+        foreach ($classes as $class) {
+            if ($hasSpecificallyAddedWidgets) {
+                if (!in_array($class, $this->availableWidgets)) {
+                    $class = '';
+                }
+            }
+            if ($hasSpecificallyBlockedWidgets) {
+                if (in_array($class, $this->blockedWidgets)) {
+                    $class = '';
+                }
+            }
+            if ($class) {
+                $widgets->push(singleton($class));
+            }
+        }
 
-		foreach($classes as $class) {
-			if($hasSpecificallyAddedWidgets) {
-				if(!in_array($class, $this->availableWidgets)) {
-					$class = '';
-				}
-			}
-			if($hasSpecificallyBlockedWidgets) {
-				if(in_array($class, $this->blockedWidgets)) {
-					$class = '';
-				}
-			}
-			if($class) {
-				$widgets->push(singleton($class));
-			}
-		}
-
-		return $widgets;
-	}
-
+        return $widgets;
+    }
 }
-
